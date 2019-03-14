2914f45361b694f99d3885b3d9586c01d3b44b4d
Klajdi Bodurri <kbontourri@gmail.com>
2019-03-14 19:31:58 +0200

    added to parser

---
+++ homework2/Token.java
@@ -0,0 +1,45 @@
+/*
+ * Describes basic struct of a token.
+ * 
+ * Contributors: Klajdi Bodurri && Eirini Tsitsopoulou.
+*/
+
+public class Token{
+
+    public static final int OPERATOR = 0;
+    public static final int NUMBER = 1;
+    public static final int ENDOFSTRING = 2;
+    public static final String OPERATORS = "+-*x/()";
+
+    private int type;
+    private String value = null;
+
+    public Token(int newType, String newValue){
+        type = newType;
+        value = newValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    
+    public int getType() {
+        return type;
+    }
+
+    // Checks whether the type is a number.
+    public boolean isNumber() {
+        if (type == NUMBER){
+            return true;
+        }
+        return false;
+    }
+    
+    // Checks whether the type is an operator.
+    public boolean isOperator() {
+        if (type == OPERATOR){
+            return true;
+        }
+        return false;
+    }
+}
